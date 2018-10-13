62ba6f23f97422e1eb7b51c4b6969c0bc1c67acd
orison09 <orison09@gmail.com>
2018-10-13 22:22:54 +0800

    Setup Credentials

--- lib/project_info.rb
+++ lib/project_info.rb
@@ -1,2 +1,17 @@
 #Creating File Project Info
 
+require 'http'
+require 'yaml'
+
+config = YAML.safe_load(File.read('config/secrets.yml'))
+
+def yahoo_api_path(path)
+    'https://developer.yahoo.com/' + path
+end
+
+def call_yahoo_url(config, url)
+    HTTP.headers(
+        'Accept' => 
+        'Authorization' => 
+    )
+end
