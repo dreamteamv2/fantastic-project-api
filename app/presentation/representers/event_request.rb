d7c4d7edbf2bd8c8b8dc148e1e78fc8270c8b832
Abner Tellez <atellezsazo@gmail.com>
2018-12-28 12:12:27 +0800

    roar

--- app/presentation/representers/event_request.rb
+++ app/presentation/representers/event_request.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'roar/decorator'
+require 'roar/json'
+
 # Represents essential Repo information for API output
 module FantasticProject
   module Representer
